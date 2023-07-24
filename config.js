83fdd7198f303435db7d58dfc547d6f29cf83984
nolight132 <71591964+nolight132@users.noreply.github.com>
2023-07-24 14:23:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -65,9 +65,9 @@ const CONFIG = {
 		},
 		{
 			id: '3',
-			name: 'Todoist',
-			icon: 'trello',
-			link: 'https://todoist.com',
+			name: 'OpenStreetMap',
+			icon: 'map-pin',
+			link: 'https://www.openstreetmap.org',
 		},
 		{
 			id: '4',

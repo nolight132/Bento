90d96e063d09747d6676256f10ed8f563417900e
nolight132 <71591964+nolight132@users.noreply.github.com>
2023-10-05 08:17:58 +0000

    Update config.js

--- config.js
+++ config.js
@@ -83,9 +83,9 @@ const CONFIG = {
 		},
 		{
 			id: '6',
-			name: 'Odysee',
-			icon: 'youtube',
-			link: 'https://odysee.com/',
+			name: 'Classroom',
+			icon: 'backpack',
+			link: 'https://classroom.google.com/u/1//',
 		},
 	],
 
